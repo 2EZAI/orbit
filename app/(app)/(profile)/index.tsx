7e44cb38b08536db29725f07ffb8372535209c2a
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2025-10-01 11:28:26 +0500

    Remove console log.

--- app/(app)/(profile)/index.tsx
+++ app/(app)/(profile)/index.tsx
@@ -2,8 +2,5 @@ import React ,{useEffect}from "react";
 import { UnifiedProfilePage } from "~/src/components/profile/UnifiedProfilePage";
 
 export default function Profile() {
-  useEffect(() => {
-    console.log("dfsasad");
-  }, []);
   return <UnifiedProfilePage />;
 }
