4331b0901faa5f8f209db802d7bfe000a9aeb23c
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2025-10-03 00:20:47 +0500

    Create unifieddetailsheet content component.

---
+++ src/components/map/UnifiedDetailsSheetContent.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { View } from "react-native";
+import { useTheme } from "~/src/components/ThemeProvider";
+import { EventDetailsSection } from "./sections/EventDetailsSection";
+import { LocationDetailsSection } from "./sections/LocationDetailsSection";
+import { TicketmasterDetailsSection } from "./sections/TicketmasterDetailsSection";
+import { EventAttendeesSection } from "./sections/EventAttendeesSection";
+import { LocationBusinessInfoSection } from "./sections/LocationBusinessInfoSection";
+import { LocationEventsSection } from "./sections/LocationEventsSection";
+import { EventSimilarSection } from "./sections/EventSimilarSection";
+import { LocationSimilarSection } from "./sections/LocationSimilarSection";
+
+export interface UnifiedDetailsSheetContentProps {
+  data: any;
+  isEventType: boolean;
+  isTicketmasterEvent: boolean;
+  isUserEvent: boolean;
+  isGoogleApiEvent: boolean;
+  isCreator: boolean;
+  isJoined: boolean;
+  hasTickets: boolean;
+  attendeeCount: number;
+  attendeeProfiles: any[];
+  locationEvents: any[];
+  loadingLocationEvents: boolean;
+  nearbyData: any[];
+  onDataSelect: (data: any) => void;
+  onShowControler: () => void;
+}
+
+export function UnifiedDetailsSheetContent({
+  data,
+  isEventType,
+  isTicketmasterEvent,
+  isUserEvent,
+  isGoogleApiEvent,
+  isCreator,
+  isJoined,
+  hasTickets,
+  attendeeCount,
+  attendeeProfiles,
+  locationEvents,
+  loadingLocationEvents,
+  nearbyData,
+  onDataSelect,
+  onShowControler,
+}: UnifiedDetailsSheetContentProps) {
+  const { theme } = useTheme();
+
+  if (isEventType) {
+    // EVENT CONTENT
+    if (isTicketmasterEvent) {
+      // TICKETMASTER EVENT CONTENT
+      return (
+        <View>
+          <TicketmasterDetailsSection data={data} />
+          <EventAttendeesSection 
+            data={data}
+            attendeeCount={attendeeCount}
+            attendeeProfiles={attendeeProfiles}
+          />
+          <EventSimilarSection 
+            data={data}
+            nearbyData={nearbyData}
+            onDataSelect={onDataSelect}
+          />
+        </View>
+      );
+    } else {
+      // REGULAR EVENT CONTENT (User Events)
+      return (
+        <View>
+          <EventDetailsSection 
+            data={data}
+            isCreator={isCreator}
+            isJoined={isJoined}
+            hasTickets={hasTickets}
+          />
+          <EventAttendeesSection 
+            data={data}
+            attendeeCount={attendeeCount}
+            attendeeProfiles={attendeeProfiles}
+          />
+          <EventSimilarSection 
+            data={data}
+            nearbyData={nearbyData}
+            onDataSelect={onDataSelect}
+          />
+        </View>
+      );
+    }
+  } else {
+    // LOCATION CONTENT
+    return (
+      <View>
+        <LocationDetailsSection data={data} />
+        <LocationBusinessInfoSection data={data} />
+        <LocationEventsSection 
+          data={data}
+          locationEvents={locationEvents}
+          loadingLocationEvents={loadingLocationEvents}
+          onDataSelect={onDataSelect}
+        />
+        <LocationSimilarSection 
+          data={data}
+          nearbyData={nearbyData}
+          onDataSelect={onDataSelect}
+        />
+      </View>
+    );
+  }
+}
