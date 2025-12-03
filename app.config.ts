db21e8ebf1a3bb430591253ef8b59f9e6b9127b8
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2025-12-04 02:08:58 +0500

    Update build version to 32.

--- app.config.ts
+++ app.config.ts
@@ -52,7 +52,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ],
     ios: {
       bundleIdentifier: "com.dovydmcnugget.orbit",
-      buildNumber: "31",
+      buildNumber: "32",
       supportsTablet: true,
       config: {
         usesNonExemptEncryption: false,
