c897ec08602444f1562d9cc23a8fea01fd8dd1a5
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2025-12-08 02:32:39 +0500

    Build version 34.

--- app.config.ts
+++ app.config.ts
@@ -52,7 +52,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ],
     ios: {
       bundleIdentifier: "com.dovydmcnugget.orbit",
-      buildNumber: "33",
+      buildNumber: "34",
       supportsTablet: true,
       config: {
         usesNonExemptEncryption: false,
