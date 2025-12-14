4d11a5e5b3151118d8934db4266875474b7c271b
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2025-12-15 00:02:51 +0500

    Update build version to 35.

--- app.config.ts
+++ app.config.ts
@@ -52,7 +52,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ],
     ios: {
       bundleIdentifier: "com.dovydmcnugget.orbit",
-      buildNumber: "34",
+      buildNumber: "35",
       supportsTablet: true,
       config: {
         usesNonExemptEncryption: false,
