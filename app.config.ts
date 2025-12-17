55aa80ad0a1a8c3e7ceb16e2df83a6e17c211dd6
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2025-12-18 00:11:14 +0500

    Update build number to 1.0.1(2).

--- app.config.ts
+++ app.config.ts
@@ -52,7 +52,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ],
     ios: {
       bundleIdentifier: "com.dovydmcnugget.orbit",
-      buildNumber: "1",
+      buildNumber: "2",
       supportsTablet: true,
       config: {
         usesNonExemptEncryption: false,
